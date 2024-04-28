f6797b379dbcd4952875745f599065ea3ea67737
Walter254 <wagudewalter2@gmail.com>
2024-04-28 12:56:29 -0400

    intergrated openai chatbot in the chat page

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
 import os
 
-
 class Config:
     MONGO_URI = os.getenv('MONGO_URI', 'mongodb+srv://wagudewalter2:wwowA2016@cluster0.6ea9hhs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
